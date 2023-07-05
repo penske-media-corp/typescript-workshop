81e36a431b99d9062137b3608fb9e82f14dab4b9
Mike Auteri <mauteri@gmail.com>
2023-07-05 11:01:41 -0400

    Refactor number.js to use TypeScript

--- src/js/number.ts
+++ src/js/number.ts
@@ -3,15 +3,15 @@
  *
  * @type {string}
  */
-const storage = 'testId';
+const storage: string = 'testId';
 
 /**
  * Helper to get the ID from localStorage.
  *
  * @returns {number}
  */
-export function getId() {
-	const id = parseInt(global.localStorage.getItem(storage), 10);
+export function getId(): number {
+	const id: number = parseInt(global.localStorage.getItem(storage), 10);
 
 	return validateId(id);
 }
@@ -22,7 +22,7 @@ export function getId() {
  * @param {number} id
  * @returns {void}
  */
-export function setId(id) {
+export function setId(id: number): void {
 	global.localStorage.setItem(storage, String(id));
 }
 
@@ -32,7 +32,7 @@ export function setId(id) {
  * @param {number} id
  * @returns {number}
  */
-export function validateId(id) {
+export function validateId(id: number): number {
 	if (isNaN(id) || 0 > id) {
 		return 0;
 	}
