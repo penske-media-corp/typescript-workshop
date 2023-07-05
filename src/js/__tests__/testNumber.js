871fd75d99ca5f56ff1643b38ad9c7ecfa1e0746
Mike Auteri <mauteri@gmail.com>
2023-07-05 10:39:29 -0400

    Feature/mauteri (#2)
    * Setup basics of repo and added first JS example to convert to TS.
    
    * Add space.
    
    * Add ID validation helper function

--- src/js/__tests__/testNumber.js
+++ src/js/__tests__/testNumber.js
@@ -4,6 +4,16 @@ test('getId is 0', () => {
 	expect(getId()).toBe(0);
 });
 
+test('getId is 0', () => {
+	setId(0);
+	expect(getId()).toBe(0);
+});
+
+test('getId is 0', () => {
+	setId(-11);
+	expect(getId()).toBe(0);
+});
+
 test('getId is 42', () => {
 	setId(42);
 	expect(getId()).toBe(42);
