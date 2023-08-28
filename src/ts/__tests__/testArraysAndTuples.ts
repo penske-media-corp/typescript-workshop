0dc1c585c1fd4bd060cd98dab7a60af128a9e6f2
Dan Berko <dberko@pmc.com>
2023-08-28 16:54:59 -0400

    PR feedback. Using JSON objects instead of strings. return type and using custom variable type in definition.

--- src/ts/__tests__/testArraysAndTuples.ts
+++ src/ts/__tests__/testArraysAndTuples.ts
@@ -1,97 +1,97 @@
 import { calculatePayStubForEmployee } from '../arraysAndTuples';
 
-test('Basic Input', () => {
-    const employeePayStub = [
-        [1, "John Doe", "john.doe@example.com", "Manager"], // employee tuple
-        [20, 40] // payment details [hourlyRate, hoursWorked]
-    ];
-    const expectedOutput = `Paystub for John Doe:
+test( 'Basic Input', () => {
+	const employeePayStub = [
+		[ 1, 'John Doe', 'john.doe@example.com', 'Manager' ], // employee tuple
+		[ 20, 40 ], // payment details [hourlyRate, hoursWorked]
+	];
+	const expectedOutput = {
+		employeeName: 'John Doe',
+		employeeID: 1,
+		email: 'john.doe@example.com',
+		title: 'Manager',
+		hoursWorked: 40,
+		rate: 20,
+		grossPay: 800,
+	};
 
-Employee ID: 1
-Email: john.doe@example.com
-Title: Manager
+	const actualOutput = calculatePayStubForEmployee( employeePayStub );
+	expect( actualOutput ).toMatchObject( expectedOutput );
+} );
 
-Hours Worked: 40
-Hourly Rate: $20.00
-Gross Pay: $800.00`;
+test( 'Missing title', () => {
+	const employeePayStub = [
+		[ 1, 'John Doe', 'john.doe@example.com' ], // employee tuple without title
+		[ 20, 40 ], // payment details [hourlyRate, hoursWorked]
+	];
+	const expectedOutput = {
+		employeeName: 'John Doe',
+		employeeID: 1,
+		email: 'john.doe@example.com',
+		title: 'N/A',
+		hoursWorked: 40,
+		rate: 20,
+		grossPay: 800,
+	};
 
-    const actualOutput = calculatePayStubForEmployee(employeePayStub);
-    expect(actualOutput).toBe(expectedOutput);
-});
+	const actualOutput = calculatePayStubForEmployee( employeePayStub );
+	expect( actualOutput ).toMatchObject( expectedOutput );
+} );
 
-test(' Missing Title', () => {
-    const employeePayStub = [
-        [1, "John Doe", "john.doe@example.com"], // employee tuple without title
-        [20, 40] // payment details [hourlyRate, hoursWorked]
-    ];
-    const expectedOutput = `Paystub for John Doe:
+test( 'Zero hoursWorked', () => {
+	const employeePayStub = [
+		[ 1, 'John Doe', 'john.doe@example.com', 'Manager' ], // employee tuple
+		[ 20, 0 ], // payment details with zero hoursWorked
+	];
+	const expectedOutput = {
+		employeeName: 'John Doe',
+		employeeID: 1,
+		email: 'john.doe@example.com',
+		title: 'Manager',
+		hoursWorked: 0,
+		rate: 20,
+		grossPay: 0,
+	};
 
-Employee ID: 1
-Email: john.doe@example.com
-Title: N/A
+	const actualOutput = calculatePayStubForEmployee( employeePayStub );
+	expect( actualOutput ).toMatchObject( expectedOutput );
+} );
 
-Hours Worked: 40
-Hourly Rate: $20.00
-Gross Pay: $800.00`;
+test( 'Negative rate', () => {
+	const employeePayStub = [
+		[ 1, 'John Doe', 'john.doe@example.com', 'Manager' ], // employee tuple
+		[ -20, 40 ], // payment details with negative rate
+	];
+	// Note that the grossPay should still be positive, since negative hours are not allowed
+	const expectedOutput = {
+		employeeName: 'John Doe',
+		employeeID: 1,
+		email: 'john.doe@example.com',
+		title: 'Manager',
+		hoursWorked: 40,
+		rate: -20,
+		grossPay: -800,
+	};
 
-    const actualOutput = calculatePayStubForEmployee(employeePayStub);
-    expect(actualOutput).toBe(expectedOutput);
-});
+	const actualOutput = calculatePayStubForEmployee( employeePayStub );
+	expect( actualOutput ).toMatchObject( expectedOutput );
+} );
 
-test('Zero Hours Worked', () => {
-    const employeePayStub = [
-        [1, "John Doe", "john.doe@example.com", "Manager"], // employee tuple
-        [20, 0] // payment details with zero hours worked
-    ];
-    const expectedOutput = `Paystub for John Doe:
+test( 'Fractional rate', () => {
+	const employeePayStub = [
+		[ 1, 'John Doe', 'john.doe@example.com', 'Manager' ], // employee tuple
+		[ 15.5, 35 ], // payment details with fractional rate
+	];
+	const expectedOutput = {
+		employeeName: 'John Doe',
+		employeeID: 1,
+		email: 'john.doe@example.com',
+		title: 'Manager',
+		hoursWorked: 35,
+		rate: 15.5,
+		grossPay: 542.5,
+	};
 
-Employee ID: 1
-Email: john.doe@example.com
-Title: Manager
-
-Hours Worked: 0
-Hourly Rate: $20.00
-Gross Pay: $0.00`;
-
-    const actualOutput = calculatePayStubForEmployee(employeePayStub);
-    expect(actualOutput).toBe(expectedOutput);
-});
-
-test('Negative Hourly Rate', () => {
-    const employeePayStub = [
-        [1, "John Doe", "john.doe@example.com", "Manager"], // employee tuple
-        [-20, 40] // payment details with negative hourly rate
-    ];
-    // Note that the gross pay should still be positive, since negative hours are not allowed
-    const expectedOutput = `Paystub for John Doe:
-
-Employee ID: 1
-Email: john.doe@example.com
-Title: Manager
-
-Hours Worked: 40
-Hourly Rate: $-20.00
-Gross Pay: $-800.00`;
-
-    const actualOutput = calculatePayStubForEmployee(employeePayStub);
-    expect(actualOutput).toBe(expectedOutput);
-});
-
-test('Fractional Hourly Rate', () => {
-    const employeePayStub = [
-        [1, "John Doe", "john.doe@example.com", "Manager"], // employee tuple
-        [15.5, 35] // payment details with fractional hourly rate
-    ];
-    const expectedOutput = `Paystub for John Doe:
-
-Employee ID: 1
-Email: john.doe@example.com
-Title: Manager
-
-Hours Worked: 35
-Hourly Rate: $15.50
-Gross Pay: $542.50`;
-
-    const actualOutput = calculatePayStubForEmployee(employeePayStub);
-    expect(actualOutput).toBe(expectedOutput);
-});
+	const actualOutput = calculatePayStubForEmployee( employeePayStub );
+	expect( actualOutput ).toMatchObject( expectedOutput );
+} );
--- src/ts/arraysAndTuples.ts
+++ src/ts/arraysAndTuples.ts
@@ -8,21 +8,19 @@ type PayCheck = [Employee, [number, number]];
 
 Calculates the gross pay for an employee based on their hourly rate and hours worked.
 
-This function takes a pay stub for an employee and calculates the gross pay.
-
 The employee's information is stored in a tuple, where the first element is the employee's ID,
 
 the second element is their name, the third element is their email, and the fourth element is their title (optional).
 
 The pay stub is also stored in a tuple, where the first element is the employee tuple and the second element is a tuple of two numbers [hourlyRate, hoursWorked].
 
-The function returns a formatted string containing the pay stub information, including the employee's name, ID, email, title (if available), hours worked, hourly rate, and gross pay.
+The function returns a JSON object containing the pay stub information, including the employee's name, ID, email, title (if available), hours worked, hourly rate, and gross pay.
 
-@param employeePayStub - A tuple containing the employee's information and the payment details.
+@param {PayCheck} employeePayStub - A tuple containing the employee's information and the payment details.
 
-@returns A formatted string representing the pay stub for the employee.
+@returns {object} A JSON object representing fields for the employee's pay stub.
 */
-export function calculatePayStubForEmployee(employeePayStub: [Employee, [number, number]]) {
+export function calculatePayStubForEmployee(employeePayStub: PayCheck): object {
     // Extract the employee tuple and payment details from the pay stub
     const employee = employeePayStub[0];
     const [hourlyRate, hoursWorked] = employeePayStub[1];
@@ -30,14 +28,16 @@ export function calculatePayStubForEmployee(employeePayStub: [Employee, [number,
     // Calculate the gross pay based on the hourly rate and hours worked
     const grossPay = hourlyRate * hoursWorked;
 
-    // Create a formatted string representing the pay stub
-    const paystub = `Paystub for ${employee[1]}:\n\n` +
-        `Employee ID: ${employee[0]}\n` +
-        `Email: ${employee[2]}\n` +
-        `Title: ${employee[3] ? employee[3] : 'N/A'}\n\n` +
-        `Hours Worked: ${hoursWorked}\n` +
-        `Hourly Rate: $${hourlyRate.toFixed(2)}\n` +
-        `Gross Pay: $${grossPay.toFixed(2)}`;
+    // Create a JSON object representing the pay stub
+	const paystub = {
+		employeeName: employee[ 1 ],
+		employeeID: employee[ 0 ],
+		email: employee[ 2 ],
+		title: employee[ 3 ] ? employee[ 3 ] : 'N/A',
+		hoursWorked: hoursWorked,
+		rate: hourlyRate,
+		grossPay: grossPay,
+	};
 
     return paystub;
 }
